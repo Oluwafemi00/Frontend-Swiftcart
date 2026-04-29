2186bfd3c7360f4f017a8f107dd807478eb75fb6
Oluwafemi00 <111207863+Oluwafemi00@users.noreply.github.com>
2026-04-29 22:45:51 +0100

    fix responsive issue

--- js/index.js
+++ js/index.js
@@ -1,8 +1,32 @@
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll(".btn-display button");
+const products = document.querySelectorAll(".product");
+const noProducts = document.getElementById("no-products");
 
-buttons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    buttons.forEach((b) => b.classList.remove("active"));
-    btn.classList.add("active");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    buttons.forEach((btn) => btn.classList.remove("active"));
+    button.classList.add("active");
+
+    const filter = button.dataset.filter;
+    let visibleCount = 0;
+    noProducts.textContent = `No products found in "${button.textContent}"`;
+
+    products.forEach((product) => {
+      const category = product.dataset.category;
+
+      if (filter === "all" || category === filter) {
+        product.style.display = "flex";
+        visibleCount++;
+      } else {
+        product.style.display = "none";
+      }
+    });
+
+    // Show or hide "No products"
+    if (visibleCount === 0) {
+      noProducts.style.display = "block";
+    } else {
+      noProducts.style.display = "none";
+    }
   });
 });
